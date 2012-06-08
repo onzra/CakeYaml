f75dfcd8df1ab3455c2d2b794c32720a47fe75bd
David Mertl <dave@onzra.com>
2012-06-08 15:52:06 -0700

    Adding copyright notices

--- Lib/Yaml.php
+++ Lib/Yaml.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Symfony Yaml component support for CakePHP
+ *
+ * ONZRA: Enterprise Development
+ * Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+ * @link          https://github.com/onzra/CakeRateLimiter CakeRateLimiter
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
 //Import all requires Symfony YAML component files
 App::import('Vendor', 'Yaml.Vendor.Yaml', array('file' => 'Yaml/Yaml.php'));
--- README.md
+++ README.md
@@ -15,3 +15,11 @@ App::import('Lib', 'Yaml.Yaml');
 use Symfony\Component\Yaml\Yaml;
 
 $data = Yaml::parse($yaml_file);
+
+Licensing
+========
+ONZRA: Enterprise Development
+Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
+
+Licensed under The MIT License
+Redistributions of files must retain the above copyright notice.
