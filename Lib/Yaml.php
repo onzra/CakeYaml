52af71d2aa8e616b2ff924a7200bb8e95e891568
dmertl <dmertl@gmail.com>
2013-03-01 11:05:13 -0800

    Adding import for Yaml unescaper class

--- Lib/Yaml.php
+++ Lib/Yaml.php
@@ -19,6 +19,7 @@ App::import('Vendor', 'Yaml.Vendor.Parser', array('file' => 'Yaml/Parser.php'));
 App::import('Vendor', 'Yaml.Vendor.Inline', array('file' => 'Yaml/Inline.php'));
 App::import('Vendor', 'Yaml.Vendor.Dumper', array('file' => 'Yaml/Dumper.php'));
 App::import('Vendor', 'Yaml.Vendor.Escaper', array('file' => 'Yaml/Escaper.php'));
+App::import('Vendor', 'Yaml.Vendor.Unescaper', array('file' => 'Yaml/Unescaper.php'));
 App::import('Vendor', 'Yaml.Vendor.ExceptionInterface', array('file' => 'Yaml/Exception/ExceptionInterface.php'));
 App::import('Vendor', 'Yaml.Vendor.DumpException', array('file' => 'Yaml/Exception/DumpException.php'));
 App::import('Vendor', 'Yaml.Vendor.ParseException', array('file' => 'Yaml/Exception/ParseException.php'));
