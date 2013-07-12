f9d71dd18306ac717209581d193575f1cb588b6f
David Mertl <dmertl@Saskatoon.local>
2013-07-12 13:46:03 -0700

    Updating plugin to only import the Yaml classes if class does not exist. Prevents conflict with PHPUnit which uses Yaml for clover code coverage.

--- Lib/Yaml.php
+++ Lib/Yaml.php
@@ -9,17 +9,22 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright     Copyright 2012, ONZRA LLC (http://www.ONZRA.com)
- * @link          https://github.com/onzra/CakeRateLimiter CakeRateLimiter
+ * @link          https://github.com/onzra/CakeYaml CakeYaml
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
  */
 
-//Import all requires Symfony YAML component files
-App::import('Vendor', 'Yaml.Vendor.Yaml', array('file' => 'Yaml/Yaml.php'));
-App::import('Vendor', 'Yaml.Vendor.Parser', array('file' => 'Yaml/Parser.php'));
-App::import('Vendor', 'Yaml.Vendor.Inline', array('file' => 'Yaml/Inline.php'));
-App::import('Vendor', 'Yaml.Vendor.Dumper', array('file' => 'Yaml/Dumper.php'));
-App::import('Vendor', 'Yaml.Vendor.Escaper', array('file' => 'Yaml/Escaper.php'));
-App::import('Vendor', 'Yaml.Vendor.Unescaper', array('file' => 'Yaml/Unescaper.php'));
-App::import('Vendor', 'Yaml.Vendor.ExceptionInterface', array('file' => 'Yaml/Exception/ExceptionInterface.php'));
-App::import('Vendor', 'Yaml.Vendor.DumpException', array('file' => 'Yaml/Exception/DumpException.php'));
-App::import('Vendor', 'Yaml.Vendor.ParseException', array('file' => 'Yaml/Exception/ParseException.php'));
+//Only import if class doesn't already exist. Prevents conflicts with external tools like PHPUnit which use Yaml. This
+//technically invalidates the unit tests since it could be on a different version of Yaml. Any differences should be
+//minor and not affect testing, but it is a possibility.
+if(!class_exists('Symfony\\Component\\Yaml\\Yaml', false)) {
+	//Import all requires Symfony YAML component files
+	App::import('Vendor', 'Yaml.Vendor.Yaml', array('file' => 'Yaml/Yaml.php'));
+	App::import('Vendor', 'Yaml.Vendor.Parser', array('file' => 'Yaml/Parser.php'));
+	App::import('Vendor', 'Yaml.Vendor.Inline', array('file' => 'Yaml/Inline.php'));
+	App::import('Vendor', 'Yaml.Vendor.Dumper', array('file' => 'Yaml/Dumper.php'));
+	App::import('Vendor', 'Yaml.Vendor.Escaper', array('file' => 'Yaml/Escaper.php'));
+	App::import('Vendor', 'Yaml.Vendor.Unescaper', array('file' => 'Yaml/Unescaper.php'));
+	App::import('Vendor', 'Yaml.Vendor.ExceptionInterface', array('file' => 'Yaml/Exception/ExceptionInterface.php'));
+	App::import('Vendor', 'Yaml.Vendor.DumpException', array('file' => 'Yaml/Exception/DumpException.php'));
+	App::import('Vendor', 'Yaml.Vendor.ParseException', array('file' => 'Yaml/Exception/ParseException.php'));
+}
